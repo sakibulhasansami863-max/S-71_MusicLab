ae83c4b0e0d96a437714f1bcaa7b28840fd2f4d7
sakibulhasansami863-max <sakibulhasansami863@gmail.com>
2026-02-19 22:16:42 +0600

    Create constants.ts

---
+++ constants/constants.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = "S-71 MusicLab";
+
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyB_7diM0Xcxtimiiaq3B3ZXiRkXhlcP9L0',
+  authDomain: 's71-musiclab.firebaseapp.com',
+  projectId: 's71-musiclab',
+  storageBucket: 's71-musiclab.firebasestorage.app',
+  messagingSenderId: '450531323764',
+  appId: '1:450531323764:web:9a53789d5c3c4a5609c621',
+  measurementId: 'G-DZSKJC7WRV'
+};
